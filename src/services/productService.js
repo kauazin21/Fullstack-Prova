24c992c2ac8bfd21523e0aef6c17fb9c8da66ba2
kauazin21 <167949109+kauazin21@users.noreply.github.com>
2026-04-13 18:30:04 -0300

    Upload src/services/productService.js via API

---
+++ src/services/productService.js
@@ -0,0 +1,25 @@
+const productRepository = require('../repositories/productRepository');
+
+class ProductService {
+  async createProduct(productData) {
+    return await productRepository.create(productData);
+  }
+
+  async getAllProducts() {
+    return await productRepository.findAll();
+  }
+
+  async getProductById(id) {
+    return await productRepository.findById(id);
+  }
+
+  async updateProduct(id, productData) {
+    return await productRepository.update(id, productData);
+  }
+
+  async deleteProduct(id) {
+    return await productRepository.delete(id);
+  }
+}
+
+module.exports = new ProductService();
